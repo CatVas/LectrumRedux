e8d73070b60e7f40bb1c9ecadcbb0cc4181e567f
Vasyl Samusiev <catvaas@googlemail.com>
2019-05-07 10:05:11 +0300

    redux-online-lesson-13: Saga logout test with redux-saga-test-plan. (#24)

--- jest/scripts/setupFiles.js
+++ jest/scripts/setupFiles.js
@@ -53,6 +53,10 @@ const fetchResponseSuccess = {
     json:   jest.fn(() => Promise.resolve(responseDataSuccess)),
 };
 
+const fetchResponseSuccess204 = {
+    status: 204,
+};
+
 const fetchResponseFail401 = {
     status: 401,
     json:   jest.fn(() => Promise.resolve(responseDataFail)),
